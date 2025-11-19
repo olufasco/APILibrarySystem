11af4bbc37edebc8c1a4cc3a38c79dd851aa0c1b
olufasco <olufasco1@gmail.com>
2025-11-19 13:53:15 +0100

    2nd wed commit

--- APILibrarySystem/Models/BaseEntity.cs
+++ APILibrarySystem/Models/BaseEntity.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace APILibrarySystem.Models
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         [Key]
         public Guid Id { get; set; } = Guid.NewGuid();
